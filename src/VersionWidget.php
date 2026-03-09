4ea573c0031c961445d06c71972ba97995e1af47
Joffrey FERRY <f.joffrey@free.fr>
2026-03-09 10:25:37 +0100

    Suppression de la méthode getDeploymentData et des commentaires associés

--- src/VersionWidget.php
+++ src/VersionWidget.php
@@ -41,40 +41,4 @@ class VersionWidget extends Widget
             ];
         });
     }
-
-
-    // public function getDeploymentData(): array
-    // {
-    //     $releaseName = 'N/A';
-
-    //     // On définit les chemins potentiels
-    //     $paths = [
-    //         base_path().'/.dep/latest_release',                 // Local ou si .dep est dans le dossier release
-    //         dirname(base_path()).'/.dep/latest_release',        // Cas standard Deployer (un niveau au dessus)
-    //         dirname(dirname(base_path())).'/.dep/latest_release', // Si base_path() est déjà profond
-    //     ];
-
-    //     foreach ($paths as $path) {
-    //         if (file_exists($path)) {
-    //             $releaseName = trim(file_get_contents($path));
-    //             break; // On a trouvé le fichier, on arrête de chercher
-    //         }
-    //     }
-
-    //     // Si on n'a toujours rien trouvé via le fichier .dep,
-    //     // on utilise le nom du dossier actuel (releases/15 -> 15)
-    //     if ($releaseName === 'N/A') {
-    //         $currentFolder = basename(base_path());
-    //         // On vérifie que c'est bien un chiffre (le numéro de release)
-    //         if (is_numeric($currentFolder)) {
-    //             $releaseName = $currentFolder;
-    //         }
-    //     }
-
-    //     return [
-    //         'date' => Carbon::createFromTimestamp(filemtime(base_path()))->translatedFormat('d F Y à H:i'),
-    //         'release' => $releaseName,
-    //         'env' => app()->environment(),
-    //     ];
-    // }
 }
